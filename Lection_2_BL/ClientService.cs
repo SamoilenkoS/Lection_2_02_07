3569cb0746bf79563ffc0b3b42d4ec188e495f96
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-07-02 12:52:30 +0300

    Implemented client logic

---
+++ Lection_2_BL/ClientService.cs
@@ -0,0 +1,22 @@
+﻿using Lection_2_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lection_2_BL
+{
+    public class ClientService : IClientService
+    {
+        public bool RentABook(Book book, Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReturnABook(Book book, Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lection_2_BL/IClientService.cs
@@ -0,0 +1,15 @@
+﻿using Lection_2_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lection_2_BL
+{
+    public interface IClientService
+    {
+        bool RentABook(Book book, Client client);
+        bool ReturnABook(Book book, Client client);
+    }
+}
