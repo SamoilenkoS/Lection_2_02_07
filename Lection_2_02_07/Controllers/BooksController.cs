7961775d4498c58747fd724e6434b36cb9115957
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-07-02 10:48:29 +0300

    Add project files.

---
+++ Lection_2_02_07/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lection_2_02_07.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
