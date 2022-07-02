780c68dc2313320ffe986eabf3b020d14cb480d0
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-07-02 10:51:38 +0300

    Implemented Books controller methods

---
+++ Lection_2_02_07/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lection_2_02_07
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PagesCount { get; set; }
+    }
+}
