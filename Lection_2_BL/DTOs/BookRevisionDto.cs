aa9b46f9a53e4309bebf3f4bd1c44a731be9580c
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-07-09 11:13:16 +0300

    Implemented full book info get

---
+++ Lection_2_BL/DTOs/BookRevisionDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lection_2_BL.DTOs
+{
+    public class BookRevisionDto
+    {
+        public int YearOfPublishing { get; set; }
+        public int PagesCount { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ Lection_2_DAL/IBooksRepository.cs
@@ -0,0 +1,12 @@
+﻿using Lection_2_DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lection_2_DAL
+{
+    public interface IBooksRepository
+    {
+        Task<(Book book, IEnumerable<BookRevision> bookRevisions)> GetFullInfo(Guid id);
+    }
+}
