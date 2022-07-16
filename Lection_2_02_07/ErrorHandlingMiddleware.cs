e9bb9de0511256ff1961b83015cb640b6f746b9c
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-07-16 12:55:54 +0300

    Added middleware for Error Handling

---
+++ Lection_2_02_07/ErrorHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lection_2_BL
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {//before endp
+                await _next(context);
+            }//after endp
+            catch (ArgumentException)
+            {
+                context.Response.StatusCode = 405;
+            }
+        }
+    }
+}
